89350fad7da6b10af85da822b6774e876a244a76
bbaumg <bbaumg@gmail.com>
2022-05-07 22:28:30 -0500

    a

--- setup.py
+++ setup.py
@@ -4,12 +4,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
 	name='garagepi',
-	version='1.0.0',
+	version='1.0.2',
 	description='Driver for sending Thingspeak data',
 	url='https://github.com/bbaumg/garagepi',
 	author='bbaumg',
 	license='MIT',
-	packages=[''],
 	install_requires=['smbus', 
-    'RPi.GPIO', 'twilio']
+    'RPi.GPIO', 'twilio', 'pyyaml', 'bme280==1.0.2', 'thingspeak==1.1.1'],
+  dependency_links=['https://github.com/bbaumg/Python_BME280/tarball/master#egg=bme280-1.0.2',
+    'https://github.com/bbaumg/Python_Thingspeak'
+  ]
 )
